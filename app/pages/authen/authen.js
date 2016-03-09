6ca2b3dc99be43b389c1f29afb4ac0203c92eef6
Hi <Hi>
2016-03-09 18:06:25 +0700

    First Step

---
+++ app/pages/authen/authen.js
@@ -0,0 +1,11 @@
+import {Page} from 'ionic-angular';
+import {forwardRef} from 'angular2/core';
+
+@Page({
+  templateUrl: 'build/pages/authen/authen.html'
+})
+export class Authentication {
+    constructor(){
+        
+    }
+}
